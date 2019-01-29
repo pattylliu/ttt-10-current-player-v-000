2db27032acb86c36825c60b1d800db4d1b863f3b
Patty Liu <pattylliu@gmail.com>
2019-01-29 20:34:23 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,13 @@
 board = [" ", " ", "X", "O", " ", "X", " ", " ", " ", " "]
-counter = 0
-
-board.each do |turn|
-  if turn != " "
-   counter += 1
-    puts "#{counter} turn(s) have been played."
-  end
-end
 
 def turn_count(board)
-  if counter 
+  counter = 0
+
+  board.each do |turn|
+    if turn != " "
+      counter += 1
+    end
+  end
+
+  counter
+end
