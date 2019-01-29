5bb251f7fdda6ba40ac7478620c1c73d93d8f9ca
Patty Liu <pattylliu@gmail.com>
2019-01-29 20:55:44 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,6 @@
 board = [" ", " ", "X", "O", " ", "X", " ", " ", " ", " "]
 
+#turn_count
 def turn_count(board)
   counter = 0
 
@@ -10,4 +11,13 @@ def turn_count(board)
   end
 
   counter
+end
+
+#current_player
+def current_player(board)
+  if turn_count(board).even?
+    current_player = "X"
+  else
+    current_player = "O"
+  end
 end
